99c06d65949bde15088b67a30f0a6ed65dd9457c
Christophe Houlet <chhoulet@yahoo.fr>
2015-05-27 15:47:38 +0200

    Creation du routing controller vue de la fonctionnalite creation d'une offre d'emploi + test

--- src/FrontOfficeEmploiBundle/Controller/EmployerController.php
+++ src/FrontOfficeEmploiBundle/Controller/EmployerController.php
@@ -4,7 +4,9 @@ namespace FrontOfficeEmploiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FrontOfficeEmploiBundle\Entity\Society;
+use FrontOfficeEmploiBundle\Entity\JobOffer;
 use FrontOfficeEmploiBundle\Form\SocietyType;
+use FrontOfficeEmploiBundle\Form\JobOfferType;
 use Symfony\Component\HttpFoundation\Request;
 
 
@@ -27,6 +29,28 @@ class EmployerController extends Controller
 			return $this -> redirect($this -> generateUrl('front_office_homepage_homepage'));
 		}
 
-		return $this -> render('FrontOfficeEmploiBundle:Employer:createSociety.html.twig', array('formSociety'=>$formSociety->createView()));
+		return $this -> render('FrontOfficeEmploiBundle:Employer:createSociety.html.twig', 
+			array('formSociety'=>$formSociety->createView()));
+	}
+
+	public function createJobOfferAction(Request $request)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$jobOffer = new JobOffer();
+		$formJobOffer = $this -> createForm(new JobOfferType(), $jobOffer);
+
+		$formJobOffer -> handleRequest($request);
+
+		if($formJobOffer -> isValid())
+		{
+			$jobOffer -> setDateCreated(new \DateTime('now'));
+			$em -> persist($jobOffer);
+			$em -> flush();
+
+			return $this -> redirect($this -> generateUrl('front_office_homepage_homepage'));
+		}
+
+		return $this -> render('FrontOfficeEmploiBundle:Employer:createJobOffer.html.twig', 
+			array('formJobOffer'=>$formJobOffer->createView()));
 	}
 }
---
+++ src/FrontOfficeEmploiBundle/Form/JobOfferType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace FrontOfficeEmploiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class JobOfferType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('descriptionJob')
+            ->add('valider','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOfficeEmploiBundle\Entity\JobOffer'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontofficeemploibundle_joboffer';
+    }
+}
