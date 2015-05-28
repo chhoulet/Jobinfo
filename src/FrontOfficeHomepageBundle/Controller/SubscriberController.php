b547604456a33c83e109a1c43f4dff5b20aa72d8
Christophe Houlet <chhoulet@yahoo.fr>
2015-05-28 13:14:41 +0200

    Generation du formaulaire FormationType + mise en forme du menu déroulant + routing

---
+++ src/FrontOfficeHomepageBundle/Controller/SubscriberController.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace FrontOfficeHomepageBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FrontOfficeHomepageBundle\Entity\Subscriber;
+use FrontOfficeHomepageBundle\Entity\Formation;
+use FrontOfficeHomepageBundle\Entity\Forum;
+use FrontOfficeHomepageBundle\Form\ForumType;
+use FrontOfficeHomepageBundle\Form\FormationType;
+use FrontOfficeHomepageBundle\Form\SubscriberType;
+
+class SubscriberController extends Controller
+{
+	public function inscriptionFormationAction()
+	{
+		
+	}
+}
---
+++ src/FrontOfficeHomepageBundle/Form/FormationType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FrontOfficeHomepageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FormationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('formationName')
+            ->add('formationType', 'choice', array('choices'=>array('devWeb' =>'Développement Web',
+                                                                    'inte'   =>'Intégration',
+                                                                    'devMob' =>'Développement d\'applications mobiles',
+                                                                    'reseaux'=>'Administration de réseaux',
+                                                                    'bdd'    =>'Gestion de Bases de Données')))
+            ->add('formationDescription')
+            ->add('valider', 'submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOfficeHomepageBundle\Entity\Formation'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontofficehomepagebundle_formation';
+    }
+}
