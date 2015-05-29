51cdd710edaad3ace3412d03a15ffc8a31eb6128
Christophe Houlet <chhoulet@yahoo.fr>
2015-05-29 10:04:33 +0200

    Creation du controller ResponseJobOffer + correction des use + view

---
+++ src/FrontOfficeEmploiBundle/Controller/ResponseJobOfferController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FrontOfficeEmploiBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FrontOfficeEmploiBundle\Entity\JobOffer;
+use FrontOfficeEmploiBundle\Entity\ResponseJobOffer;
+use FrontOfficeEmploiBundle\Entity\ResponseJobOfferType;
+use FrontOfficeEmploiBundle\Entity\Cuvitae;
+use FrontOfficeEmploiBundle\Entity\Candidat;
+use FrontOfficeEmploiBundle\Entity\MotivationLetter;
+use FrontOfficeEmploiBundle\Form\JobOfferType;
+use Symfony\Component\HttpFoundation\Request;
+
+class ResponseJobOfferController extends Controller
+{
+	public function responseJobOfferAction(Request $request, $id, $id_LM, $id_cuvitae, $id_candidat)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$responseJobOffer = new ResponseJobOffer();
+		$motivationLetter = $em -> getRepository('FrontOfficeEmploiBundle:MotivationLetter')->find($id_LM);
+		$jobOffer = $em -> getRepository('FrontOfficeEmploiBundle:JobOffer')->find($id);
+		$cuvitae = $em -> getRepository('FrontOfficeEmploiBundle:Cuvitae')->find($id_cuvitae);
+		$candidat =  $em -> getRepository('FrontOfficeEmploiBundle:Candidat')->find($id_candidat);
+
+		$formResponseJobOffer = $this -> createForm(new ResponseJobOfferType(), $responseJobOffer);
+
+		$formResponseJobOffer -> handleRequest($request);
+
+		if($formResponseJobOffer -> isValid())
+		{
+			$responseJobOffer -> addCuvitae($cuvitae);
+			$responseJobOffer -> addMotivationLetter($motivationLetter);
+			$responseJobOffer -> setDateCreated(new \DateTime('now'));
+			$responseJobOffer -> setJobOffer($jobOffer);
+			$responseJobOffer -> setCandidat($candidat);
+			$em ->persist($responseJobOffer);
+			$em -> flush();
+
+			return $this -> redirect($this -> generateUrl('front_office_emploi_jobOffer_showOne'));
+		}
+
+		return $this ->render('FrontOfficeEmploiBundle:ResponseJobOffer:responseJobOffer.html.twig', 
+			array('formResponseJobOffer'=>$formResponseJobOffer->createView()));
+	}
+}
--- src/FrontOfficeEmploiBundle/Entity/ResponseJobOfferRepository.php
+++ src/FrontOfficeEmploiBundle/Entity/ResponseJobOfferRepository.php
@@ -12,4 +12,5 @@ use Doctrine\ORM\EntityRepository;
  */
 class ResponseJobOfferRepository extends EntityRepository
 {
+	
 }
