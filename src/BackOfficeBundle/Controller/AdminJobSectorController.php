e5a88c44bcd288d697298da3177bae18c1387a0b
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-04 23:27:59 +0200

    Creation de la fonction list de JobSector + View

--- src/BackOfficeBundle/Controller/AdminJobSectorController.php
+++ src/BackOfficeBundle/Controller/AdminJobSectorController.php
@@ -16,4 +16,23 @@ class AdminJobSectorController extends Controller
 
 		return $this -> render('BackOfficeBundle:AdminJobSector:list.html.twig', array('listJobSector'=>$listJobSector));
 	}
+
+	public function createAction(Request $request)
+	{
+		$em = $this -> getDoctrine()-> getManager();
+		$jobSector = new JobSector();
+		$formJob = $this -> createForm(new JobSectorType(), $jobSector);
+
+		$formJob -> handleRequest($request);
+
+		if ($formJob -> isValid())
+		{
+			$em -> persist($jobSector);
+			$em -> flush();
+
+			return $this -> redirect($this->generateUrl('back_office_adminJobSector_list'));
+		}
+
+		return $this -> render('BackOfficeBundle:AdminJobSector:create.html.twig', array('formJob'=>$formJob->createView()));
+	}
 }
