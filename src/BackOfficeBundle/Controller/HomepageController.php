7bff9f826c7f9c1a14ad0947f84c1786a94a5a04
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-11 19:46:42 +0200

    Creation du SubscriberController

--- src/BackOfficeBundle/Controller/HomepageController.php
+++ src/BackOfficeBundle/Controller/HomepageController.php
@@ -8,15 +8,15 @@ class HomepageController extends Controller
 {
 	public function homepageAction()
 	{
-		$em          = $this -> getDoctrine()->getManager();
-		$nbCandidats = $em -> getRepository('FrontOfficeEmploiBundle:Candidat') -> nbCandidats();
-		$nbJobOffers = $em -> getRepository('FrontOfficeEmploiBundle:JobOffer') -> nbJobOffers();
-		$nbSociety   = $em -> getRepository('FrontOfficeEmploiBundle:Society') -> nbSociety();
-		$nbComments  = $em -> getRepository('FrontOfficeHomepageBundle:Comment') -> nbComments();
+		$em                   = $this -> getDoctrine()->getManager();
+		$nbCandidats          = $em -> getRepository('FrontOfficeEmploiBundle:Candidat') -> nbCandidats();
+		$nbJobOffers          = $em -> getRepository('FrontOfficeEmploiBundle:JobOffer') -> nbJobOffers();
+		$nbSociety            = $em -> getRepository('FrontOfficeEmploiBundle:Society') -> nbSociety();
+		$nbComments           = $em -> getRepository('FrontOfficeHomepageBundle:Comment') -> nbComments();
 		$getNbFormationByType = $em -> getRepository('FrontOfficeHomepageBundle:Formation') -> getNbFormationByType();
-		$nbFormations = $em -> getRepository('FrontOfficeHomepageBundle:Formation') -> nbFormations();
-		$nbForums    = $em -> getREpository('FrontOfficeHomepageBundle:Forum') -> nbForums();
-		$nbEta     = $em -> getRepository('FrontOfficeHomepageBundle:Etablissement') -> nbEtablissements();
+		$nbFormations         = $em -> getRepository('FrontOfficeHomepageBundle:Formation') -> nbFormations();
+		$nbForums             = $em -> getREpository('FrontOfficeHomepageBundle:Forum') -> nbForums();
+		$nbEta                = $em -> getRepository('FrontOfficeHomepageBundle:Etablissement') -> nbEtablissements();
 
 		return $this -> render('BackOfficeBundle:Homepage:homepage.html.twig', 
 			array('nbCandidats' => $nbCandidats,
