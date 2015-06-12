0fcaa2451b5438f406575a8b0f2300cc11286db0
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-12 23:30:40 +0200

    Creation de la function edit de l'entity Society + creation d'un nouvel attribut + template

--- src/BackOfficeBundle/Controller/AdminSocietyController.php
+++ src/BackOfficeBundle/Controller/AdminSocietyController.php
@@ -3,6 +3,9 @@
 namespace BackOfficeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FrontOfficeEmploiBundle\Entity\Society;
+use FrontOfficeEmploiBundle\Form\SocietyType;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminSocietyController extends Controller
 {
@@ -14,4 +17,26 @@ class AdminSocietyController extends Controller
 		return $this -> render('BackOfficeBundle:AdminSociety:triSociety.html.twig', 
 			array('triSociety' => $triSociety));
 	}
+
+	public function editAction(Request $request, $id)
+	{
+		$em = $this -> getDoctrine()->getmanager();
+		$society = $em -> getRepository('FrontOfficeEmploiBundle:Society') -> find($id);
+		$formSociety = $this -> createForm(new SocietyType(), $society);
+
+		$formSociety -> handleRequest($request);
+		if($formSociety -> isValid())
+		{
+			$society -> setdateUpdated(new \datetime('now'));
+			$em -> persist($society);
+			$em -> flush();
+
+			return $this -> redirect($this->generateUrl('back_office_adminSociety_list'));
+		}
+
+		return $this -> render('BackOfficeBundle:AdminSociety:edit.html.twig', 
+			array('formSociety' => $formSociety ->createView()));
+	}
+
+
 }
