d07f2ee01b99cb5f090305c6ff6738285935e341
Christophe Houlet <chhoulet@yahoo.fr>
2015-07-09 18:01:39 +0200

    Creation de l'entity Message + generation du formulaire

---
+++ src/FrontOfficeHomepageBundle/Form/MessageType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FrontOfficeHomepageBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MessageType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('author')
+            ->add('subject')
+            ->add('email')
+            ->add('content')
+            ->add('envoyer','submit');
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOfficeHomepageBundle\Entity\Message'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontofficehomepagebundle_message';
+    }
+}
