2a022a407c192371bfe565d78fd113ad967bc588
Christophe Houlet <chhoulet@yahoo.fr>
2015-07-09 21:55:27 +0200

    Creation du routing + Controller + view de la function contact - enregistrement de message en BDD

--- src/FrontOfficeHomepageBundle/Controller/StaticController.php
+++ src/FrontOfficeHomepageBundle/Controller/StaticController.php
@@ -3,6 +3,9 @@
 namespace FrontOfficeHomepageBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FrontOfficeHomepageBundle\Entity\Message;
+use FrontOfficeHomepageBundle\Form\MessageType;
+use Symfony\Component\HttpFoundation\Request;
 
 class StaticController extends Controller
 {
@@ -14,5 +17,26 @@ class StaticController extends Controller
 	public function conditionsAction()
 	{
 		return $this-> render('FrontOfficeHomepageBundle:Static:conditions.html.twig');
+	}
+
+	public function contactAction(Request $request)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$message = new Message();
+		$form = $this -> createForm(new MessageType(), $message);
+
+		$form ->handleRequest($request);
+
+		if ($form -> isValid())
+		{
+			$message -> setDateCreated(new \datetime());
+			$em -> persist($message);
+			$em -> flush();
+
+			return $this -> redirect($this -> generateurl('front_office_homepage_homepage'));
+		}
+
+		return $this->render('FrontOfficeHomepageBundle:Static:contact.html.twig', 
+			array('form'=>$form->createView()));
 	}
 }
