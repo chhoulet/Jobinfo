b610e36e3ce130306be5d0e4fea12bdaefff81e4
Christophe Houlet <chhoulet@yahoo.fr>
2015-09-10 10:13:43 +0200

    Adaptation du construct lors de la creation d'un compte User

--- src/FrontOfficeUserBundle/Entity/User.php
+++ src/FrontOfficeUserBundle/Entity/User.php
@@ -112,7 +112,7 @@ class User extends BaseUser
     public function __construct()
     {
         parent::__construct();
-        // your own logic
+        $this->setPersonnalSpaceActive(false);
     }
 
     /**
